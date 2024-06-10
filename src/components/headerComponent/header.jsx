d130c6c71f5d4e41c8621a691a15b6e089624a24
VishwesH <vishweshgupta30890@gmail.com>
2024-06-10 14:57:04 -0700

    stripping effect of header is done

--- src/components/headerComponent/header.jsx
+++ src/components/headerComponent/header.jsx
@@ -3,12 +3,27 @@ import "./header.css";
 import { MdMailOutline } from "react-icons/md";
 import { FaLongArrowAltDown } from "react-icons/fa";
 import { MdOutlineAlternateEmail } from "react-icons/md";
+import {useState, useEffect} from 'react'
 
 export default function Header() {
+
+  const [scrolled, setScrolled] = useState(false);
+
+    function scrollEffect() {
+      const checkedScroll = 1;
+      if (window.scrollY > checkedScroll) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    }
+
+    window.addEventListener('scroll', scrollEffect);
+
   return (
     <div className="headerComponent">
       <div className="headerComponentDiv1">
-        <h1>INNOVATE</h1>
+        <h1 className={scrolled?'innovate':''}>INNOVATE</h1>
       </div>
       <div className="headerComponentDiv2">
         <h1
